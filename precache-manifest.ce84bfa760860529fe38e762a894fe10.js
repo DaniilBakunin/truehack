3d8482cf58091120e3ba3e134cff2492ad8f1165
Daniil <89848859+DaniilBakunin@users.noreply.github.com>
2024-05-08 22:11:27 +0300

    Add files via upload

---
+++ precache-manifest.ce84bfa760860529fe38e762a894fe10.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b87f9956bcd03bb2f5180dd295edd6",
+    "url": "/truetechhack.git/index.html"
+  },
+  {
+    "revision": "a8863f4092c489a5d8e6",
+    "url": "/truetechhack.git/static/css/main.716facde.chunk.css"
+  },
+  {
+    "revision": "91c03afcbbd9c0f9626b",
+    "url": "/truetechhack.git/static/js/2.f66fa5c4.chunk.js"
+  },
+  {
+    "revision": "a8863f4092c489a5d8e6",
+    "url": "/truetechhack.git/static/js/main.6d9da268.chunk.js"
+  },
+  {
+    "revision": "b4db8845d6abc8902b50",
+    "url": "/truetechhack.git/static/js/runtime~main.6f2958ba.js"
+  },
+  {
+    "revision": "44d5ed624d531a8f48a3ef1de166439b",
+    "url": "/truetechhack.git/static/media/Logo.44d5ed62.svg"
+  },
+  {
+    "revision": "dd88fcb40311b21690bb0c8e780e9777",
+    "url": "/truetechhack.git/static/media/arrow.dd88fcb4.svg"
+  },
+  {
+    "revision": "ad413a1f3c5fed65a3e3a44a3d57ddb3",
+    "url": "/truetechhack.git/static/media/mts_bold.ad413a1f.woff2"
+  },
+  {
+    "revision": "f83bbcd01480bcc1857258ac08376000",
+    "url": "/truetechhack.git/static/media/mts_regular.f83bbcd0.woff2"
+  },
+  {
+    "revision": "7904f0b0a47c35b397ae4e600b4bf25a",
+    "url": "/truetechhack.git/static/media/search.7904f0b0.svg"
+  }
+]);
